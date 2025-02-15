e021b0e9f01712ed2a6949b8e2562bd675feeed6
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2025-02-15 18:16:46 +0530

    entity changes

--- src/main/java/com/iris/entitymanager/dto/EntityRequestDto.java
+++ src/main/java/com/iris/entitymanager/dto/EntityRequestDto.java
@@ -1,30 +1,59 @@
-package com.iris.entitymanager.dto;
-
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
-
-import java.util.Date;
-
-public class EntityRequestDto {
-    @Pattern(regexp = "^[a-zA-Z1-9.&]{200}$")
-    @Size(min=1, max=200)
-    private String entityName;
-
-    private String entityShortname;
-    private String entityCode;
-    private String ifscCode;
-    private String comTypeId;
-    private String categoryId;
-    private String subCategoryId;
-    private String entityEmailId;
-    private boolean isActive;
-    private int createdBy;
-    private Date createdOn;
-    private int lastModifiedBy;
-    private Date lastModifiedOn;
-    private String entityPhoneNo;
-    private Date updatedOn;
-    private String entityNameBil;
-    private String entityShortName;
-    private int bankType;
-}
+//package com.iris.entitymanager.dto;
+//
+//import jakarta.validation.constraints.NotEmpty;
+//import jakarta.validation.constraints.NotNull;
+//import jakarta.validation.constraints.Pattern;
+//import jakarta.validation.constraints.Size;
+//
+//import java.util.Date;
+//
+//public class EntityRequestDto {
+//    @NotEmpty
+//    @Pattern(regexp = "^[a-zA-Z1-9.&]{200}$")
+//    @Size(min=1, max=200)
+//    private String entityName;
+//
+//    @NotEmpty
+//    @Pattern(regexp = "^[a-zA-Z1-9.&]{100}$")
+//    @Size(min=1, max=100)
+//    private String entityShortname;
+//
+//    @NotEmpty
+//    @Pattern(regexp = "^[a-zA-Z0-9.&]$")
+//    @Size(min=4, max=24)
+//    private String entityCode;
+//
+//    @NotEmpty
+//    @Pattern(regexp = "^[a-zA-Z0-9.&]$")
+//    @Size(min=4, max=24)
+//    private String ifscCode;
+//
+//    //private String comTypeId;
+//
+//    private int categoryId;
+//    private int subCategoryId;
+//
+//    @Pattern(regexp = "^[a-zA-Z0-9_+&*-]+(?:\\\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\\\.)+[a-zA-Z]{2,7}$")
+//    private String entityEmailId;
+//
+//    private boolean isActive=true;
+//
+//    private int createdBy;
+//
+//    private Date createdOn;
+//
+//    private int lastModifiedBy;
+//
+//    private Date lastModifiedOn;
+//
+//    @Pattern(regexp = "^(\\\\+\\\\d{1,4})?\\\\d{10,15}$")
+//    private String entityPhoneNo;
+//
+//    private Date updatedOn;
+//
+//    private String entityNameBil=entityName;
+//
+//    private String entityShortName=entityShortname;
+//
+//    private int bankType;
+//}
--- src/main/java/com/iris/entitymanager/entity/Entityentity.java
+++ src/main/java/com/iris/entitymanager/entity/Entityentity.java
@@ -3,12 +3,69 @@ package com.iris.entitymanager.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Pattern;
 
+import java.util.Date;
+
 @Entity
+@Table(name = "TBL_ENTITY")
 public class Entityentity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
     private int id;
 
-    private String entity_name;
+    @Column(name = "ID")
+    private String entityName;
+
+    @Column(name = "ID")
+    private String entityShortname;
+
+    @Column(name = "ID")
+    private String entityCode;
+
+    @Column(name = "ID")
+    private String ifscCode;
+
+    @Column(name = "ID")
+    private String comTypeId;
+
+    @Column(name = "ID")
+    private String categoryId;
+
+    @Column(name = "ID")
+    private String subCategoryId;
+
+    @Column(name = "ID")
+    private String entityEmailId;
+
+    @Column(name = "ID")
+    private boolean isActive;
+
+    @Column(name = "ID")
+    private int createdBy;
+
+    @Column(name = "ID")
+    private Date createdOn;
+
+    @Column(name = "ID")
+    private int lastModifiedBy;
+
+    @Column(name = "ID")
+    private Date lastModifiedOn;
+
+    @Column(name = "ID")
+    private String entityPhoneNo;
+
+    @Column(name = "ID")
+    private Date updatedOn;
+
+    @Column(name = "ID")
+    private String entityNameBil;
+
+    @Column(name = "")
+    private String entityShortName;
+
+    @Column(name = "BANK_TYPE")
+    private int bankType;
+
 }
