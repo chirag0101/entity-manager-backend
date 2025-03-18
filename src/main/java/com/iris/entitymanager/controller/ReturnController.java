2c160f8a7f5eb982324fff77cb6cd8c8277b33dd
chirag.suryawanshi <chiragsuryavanshi01@gmail.com>
2025-03-18 12:38:44 +0530

    return-service-mod-history-update

--- src/main/java/com/iris/entitymanager/controller/ReturnController.java
+++ src/main/java/com/iris/entitymanager/controller/ReturnController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("CIMSCustomizationService/service")
+@RequestMapping("/service")
 public class ReturnController {
 
     @Autowired
